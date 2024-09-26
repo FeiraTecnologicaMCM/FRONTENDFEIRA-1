3f4f31c0fef2c98509b01c021afcd9eaa0d505af
Ariany Andrade <100931444+arianysant@users.noreply.github.com>
2024-09-26 11:50:39 -0300

    Add files via upload

---
+++ FrontEndFeira/app/public/src/ods.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php
+        include('../assets/components/head.php')
+    ?>
+    <!-- !AQUI VAI O CSS DA SUA PÁGINA! -->
+    <link rel="stylesheet" href="../assets/css/ods.css">
+</head>
+<body>
+    <!-- !AQUI DEVE DAR INCLUDE NO HEADER EM TODAS AS PÁGINAS MENOS NA: OPEN! -->
+    <?php
+        include('../assets/components/header.php')
+    ?>
+      
+    <!-- !AQUI VAI O CONTEÚDO DA SUA PÁGINA! -->
+    <div class="odscontainer">
+        <h1 class="titulo">O que são ODS’s?</h1>
+        <br>
+        <p>Os Objetivos de Desenvolvimento Sustentável (ODS) (ou Objetivos Mundiais para o Desenvolvimento Sustentável) são uma coleção de 17 metas globais, estabelecidas pela Assembleia Geral das Nações Unidas. Os ODS são parte da Resolução 70/1 da Assembleia Geral das Nações Unidas: "Transformando o nosso mundo: a Agenda 2030 para o Desenvolvimento Sustentável", que depois foi encurtado para Agenda 2030. As metas são amplas e interdependentes, mas cada uma tem uma lista separada de metas a serem alcançadas. Atingir todos os 169 alvos indicaria a realização de todos os 17 objetivos. Os ODS abrangem questões de desenvolvimento social e econômico, incluindo pobreza, fome, saúde, educação, aquecimento global, igualdade de gênero, água, saneamento, energia, urbanização, meio ambiente e justiça social.</p>
+        <div class="odsimg">
+            <img src="../assets/img/ods/ods 01.png" alt="ods imagem">
+        </div>
+        
+        <h2 class="subtitulo">1.Erradicar a pobreza</h2>
+        <div class="texto-imagem">
+            <img src="../assets/img/ods/ODS1pob.jpg" alt="ods imagem 2" class="imagem-secundaria">
+            <div class="texto-secundario">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat et deleniti magni excepturi quod accusamus corporis dolores totam saepe minus quam, dolorum ut? Mollitia fugit minima magni quaerat deserunt expedita.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- !AQUI VAI O JS DA SUA PÁGINA (CASO TENHA)! -->
+    <script src=""></script>
+</body>
+</html>
